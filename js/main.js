1fbde18e4e78b28cddd022ec1c23449398dd4daa
amilabbasov02 <amilabbas451.19@gmail.com>
2023-11-20 19:11:43 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.main-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items: 1,
+    dots: false,
+    navText: ["<img src='./img/left-icon.svg'>","<img src='./img/right-icon.svg'>"],
+    animateOut: 'fadeOut'
+})
